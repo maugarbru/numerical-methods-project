145f4b56e526b3bb7c9877825cccb8810fc1dc25
Mauricio G <mgarzon@celerik.com>
2021-11-09 07:08:02 -0500

    feat(backend): parseo valores y resultado

--- backend/app/views.py
+++ backend/app/views.py
@@ -7,15 +7,15 @@ from django.http import JsonResponse
 class IntegralsMethod(View):
     def post(self, request):
         data = json.loads(request.body)
-        result = simpson(data['a'], data['b'], data['n'], data['f'])
-        result_dict = { 'result': result}
+        result = simpson(int(data['a']), int(data['b']), int(data['n']), data['f'])
+        result_dict = { 'resultado': result}
         data.update(result_dict)
         return JsonResponse(data, safe=False)
 
 class DerivatesMethod(View):
     def post(self, request):
         data = json.loads(request.body)
-        result = centradas(data['x'], data['delta'], data['f'])
-        result_dict = { 'result': result}
+        result = centradas(int(data['x']), float(data['delta']), data['f'])
+        result_dict = { 'resultado': result}
         data.update(result_dict)
         return JsonResponse(data, safe=False)
