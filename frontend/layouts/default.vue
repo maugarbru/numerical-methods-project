dc05c397dfa16c1c9b95b8534d5ea167f14d43dd
Mauricio G <mgarzon@celerik.com>
2021-11-08 20:18:00 -0500

    feat(frontend): adds frontend v1 structure

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-app-bar
+      fixed
+      app
+    >
+      <v-spacer />
+      <v-toolbar-title>Metodos Númericos</v-toolbar-title>
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer
+      absolute
+      app
+    >
+    <v-spacer />
+      <span>Metodos Númericos. UdeM. Mauricio Garzon & Julian Tamayo &copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
